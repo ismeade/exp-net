058d1afcaf33d83cf93666dbb5644b966d54ef25
ismeade <ismeade99@sina.com>
2017-03-30 14:55:23 +0800

    update

--- mina-sshd/src/main/java/com/ade/exp/mina/sshd/SshServerExp.java
+++ mina-sshd/src/main/java/com/ade/exp/mina/sshd/SshServerExp.java
@@ -1,15 +1,12 @@
 package com.ade.exp.mina.sshd;
 
-import org.apache.sshd.common.Factory;
-import org.apache.sshd.server.*;
+import org.apache.sshd.common.util.OsUtils;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.config.keys.DefaultAuthorizedKeysAuthenticator;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
-import org.apache.sshd.server.scp.ScpCommandFactory;
 import org.apache.sshd.server.shell.ProcessShellFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -24,9 +21,18 @@ public class SshServerExp {
         sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(new File("hostkey.ser")));
         // 测试 一律通过
         sshd.setPasswordAuthenticator((s, s2, serverSession) -> "guest".equals(s));
-        sshd.setPublickeyAuthenticator((s, publicKey, serverSession) -> "guest".equals(s));
+//        sshd.setPublickeyAuthenticator((s, publicKey, serverSession) -> "guest".equals(s));
+        //use file ~/.ssh/authorized_keys
+        sshd.setPublickeyAuthenticator(new DefaultAuthorizedKeysAuthenticator(false));
 
-        sshd.setShellFactory(MyCommand::new);
+//        sshd.setShellFactory(new ProcessShellFactory(OsUtils.WINDOWS_COMMAND));
+
+        if (OsUtils.isUNIX()) {
+            sshd.setShellFactory(new ProcessShellFactory(OsUtils.LINUX_COMMAND));
+        } else {
+            sshd.setShellFactory(MyCommand::new);
+        }
+        
 //        sshd.setCommandFactory(s -> new MyCommand());
         try {
             sshd.start();
